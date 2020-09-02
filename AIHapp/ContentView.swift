1f962324b2b2717288e4c0d92c38c59ec20c8306
Benjamin <benjamin.cai@aihtech.com>
2020-09-02 11:40:04 -0400

    Initial Commit

---
+++ AIHapp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AIHapp
+//
+//  Created by Cai Huichuan on 2020-09-02.
+//  Copyright © 2020 Cai Huichuan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
